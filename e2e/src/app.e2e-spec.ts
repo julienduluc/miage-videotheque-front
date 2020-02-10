436e98e1bf403d6dd864ac59457a40e2434608aa
Robin Bobo <robin.bobo@scalian.com>
2020-02-10 14:29:55 +0100

    fix: e2e tests

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -11,4 +11,12 @@ describe('workspace-project App', () => {
     page.navigateTo();
     expect(page.getAppText()).toEqual('MyApp');
   });
+
+  /* afterEach(async () => {
+     // Assert that there are no errors emitted from the browser
+     const logs = await browser.manage().logs().get(logging.Type.BROWSER);
+     expect(logs).not.toContain(jasmine.objectContaining({
+       level: logging.Level.SEVERE,
+     } as logging.Entry));
+   });*/
 });
