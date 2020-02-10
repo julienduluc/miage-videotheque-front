c2d17e12e24f1e41ddddf3ad2dadb8d04204ebc7
Robin Bobo <robin.bobo@scalian.com>
2020-02-10 11:15:17 +0100

    remove: ng-pick-datetime

--- src/app/shared/shared-libs.module.ts
+++ src/app/shared/shared-libs.module.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { NgModule } from '@angular/core';
+import { ModuleWithProviders, NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import {
   MatButtonModule,
